e997688e2f7de2b053cc95bcf3023b636c40bc1a
dev-redo <mis05041@naver.com>
2024-03-13 16:06:39 +0900

    feat: init tooltip

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -19,10 +19,11 @@ type TooltipStylesProps = Omit<TooltipProps, 'children' | 'content'>;
 export function Tooltip({ direction = 'top', withArrow = true, children, content }: TooltipProps) {
   return (
     <TooltipContainer>
-      {children}
-      <TooltipBox className="tooltip-box" withArrow={withArrow} direction={direction}>
-        {content}
-      </TooltipBox>
+      <ContentsWrapper>{children}</ContentsWrapper>
+      <TooltipWrapper className="tooltip-box">
+        <TooltipArrow />
+        <TooltipBubble>{content}</TooltipBubble>
+      </TooltipWrapper>
     </TooltipContainer>
   );
 }
@@ -38,112 +39,152 @@ const TooltipContainer = styled.div`
   }
 `;
 
-const TOOLTIP_ARROW_WIDTH = 9;
-const TOOLTIP_ARROW_HEIGHT = 5;
-const DISTANCE_OF_ARROW_ELEMENT = 4;
+const TooltipWrapper = styled.div`
+  /* visibility: hidden;
+  opacity: 0; */
+`;
 
-const directionStylesheet = ({
-  direction,
-  withArrow,
-}: {
-  direction?: Direction;
-  withArrow?: boolean;
-}) => {
-  const directionStyle = {
-    bubblePosition: `calc(100% + ${
-      withArrow ? TOOLTIP_ARROW_HEIGHT + DISTANCE_OF_ARROW_ELEMENT : DISTANCE_OF_ARROW_ELEMENT
-    }px)`,
-    arrowPosition: `calc(100% - ${DISTANCE_OF_ARROW_ELEMENT}px)`,
-    arrowRadius: '2px',
-  } as const;
+const ContentsWrapper = styled.div`
+  display: flex;
+`;
 
-  switch (direction) {
-    case 'top':
-      return css`
-        bottom: ${directionStyle.bubblePosition};
-        left: 50%;
-        transform: translate(-50%, 1px);
+const TOOLTIP_ARROW_WIDTH = 10;
 
-        &::after {
-          top: ${directionStyle.arrowPosition};
-          left: 50%;
-          transform: translate(-50%, -1px) rotate(45deg);
-          border-bottom-right-radius: ${directionStyle.arrowRadius};
-        }
-      `;
-    case 'bottom':
-      return css`
-        top: ${directionStyle.bubblePosition};
-        left: 50%;
-        transform: translate(-50%, 1px);
-
-        &::after {
-          bottom: ${directionStyle.arrowPosition};
-          left: 50%;
-          transform: translate(-50%, 1px) rotate(45deg);
-          border-top-left-radius: ${directionStyle.arrowRadius};
-        }
-      `;
-    case 'left':
-      return css`
-        top: 50%;
-        right: ${directionStyle.bubblePosition};
-        transform: translate(1px, -50%);
-
-        &::after {
-          top: 40%;
-          left: ${directionStyle.arrowPosition};
-          transform: translateX(-1px) rotate(45deg);
-          border-top-right-radius: ${directionStyle.arrowRadius};
-        }
-      `;
-    case 'right':
-      return css`
-        top: 50%;
-        left: ${directionStyle.bubblePosition};
-        transform: translate(1px, -50%);
-
-        &::after {
-          top: 40%;
-          right: ${directionStyle.arrowPosition};
-          transform: translateX(1px) rotate(45deg);
-          border-bottom-left-radius: ${directionStyle.arrowRadius};
-        }
-      `;
-    default:
-      return css``;
-  }
-};
-
-const TooltipBox = styled.div<TooltipStylesProps>`
-  position: relative;
-  cursor: help;
-  z-index: 10000;
-  opacity: 0;
-
-  ${({ direction, withArrow }) => directionStylesheet({ direction, withArrow })}
-
+const TooltipArrow = styled.div`
   position: absolute;
-  visibility: hidden;
+  bottom: calc(100% + 2px);
+  left: calc(50% - ${TOOLTIP_ARROW_WIDTH / 2}px);
+  transform: translateX(-50%);
+  background: ${color['main-black']};
+  width: ${TOOLTIP_ARROW_WIDTH}px;
+  height: ${TOOLTIP_ARROW_WIDTH}px;
+  border-bottom-right-radius: 2px;
+  transform: rotate(45deg);
+`;
+
+const TooltipBubble = styled.div`
+  position: absolute;
+  bottom: calc(100% + 20px); // 시작 높이: 입힐 컴포넌트 + arrow 전체 height
+  left: calc(50%);
+  transform: translateX(-25px); // 툴팁의 절반인 10px + 툴팁과 bubble사이 거리 15px
   width: max-content;
   height: max-content;
-
+  max-width: 200px;
+  cursor: help;
   background: ${color['main-black']};
   color: ${color.white};
   font-size: ${typography.size.xxs}px;
   font-weight: ${typography.weight.regular};
   padding: 7px 10px;
   border-radius: 7px;
-
-  ${({ withArrow }) =>
-    withArrow &&
-    css`
-      &::after {
-        content: '';
-        position: absolute;
-        width: ${TOOLTIP_ARROW_WIDTH}px;
-        height: ${TOOLTIP_ARROW_WIDTH}px; // arrow의 base인 정사각형 세로 height
-        background: ${color['main-black']};
-      }
-    `}
 `;
+
+// const TOOLTIP_ARROW_WIDTH = 9;
+// const TOOLTIP_ARROW_HEIGHT = 5;
+// const DISTANCE_OF_ARROW_ELEMENT = 4;
+
+// const directionStylesheet = ({
+//   direction,
+//   withArrow,
+// }: {
+//   direction?: Direction;
+//   withArrow?: boolean;
+// }) => {
+//   const directionStyle = {
+//     bubblePosition: `calc(100% + ${
+//       withArrow ? TOOLTIP_ARROW_HEIGHT + DISTANCE_OF_ARROW_ELEMENT : DISTANCE_OF_ARROW_ELEMENT
+//     }px)`,
+//     arrowPosition: `calc(100% - ${DISTANCE_OF_ARROW_ELEMENT}px)`,
+//     arrowRadius: '2px',
+//   } as const;
+
+//   switch (direction) {
+//     case 'top':
+//       return css`
+//         bottom: ${directionStyle.bubblePosition};
+//         left: 50%;
+//         transform: translate(-50%, 1px);
+
+//         &::after {
+//           top: ${directionStyle.arrowPosition};
+//           left: 50%;
+//           transform: translate(-50%, -1px) rotate(45deg);
+//           border-bottom-right-radius: ${directionStyle.arrowRadius};
+//         }
+//       `;
+//     case 'bottom':
+//       return css`
+//         top: ${directionStyle.bubblePosition};
+//         left: 50%;
+//         transform: translate(-50%, 1px);
+
+//         &::after {
+//           bottom: ${directionStyle.arrowPosition};
+//           left: 50%;
+//           transform: translate(-50%, 1px) rotate(45deg);
+//           border-top-left-radius: ${directionStyle.arrowRadius};
+//         }
+//       `;
+//     case 'left':
+//       return css`
+//         top: 50%;
+//         right: ${directionStyle.bubblePosition};
+//         transform: translate(1px, -50%);
+
+//         &::after {
+//           top: 40%;
+//           left: ${directionStyle.arrowPosition};
+//           transform: translateX(-1px) rotate(45deg);
+//           border-top-right-radius: ${directionStyle.arrowRadius};
+//         }
+//       `;
+//     case 'right':
+//       return css`
+//         top: 50%;
+//         left: ${directionStyle.bubblePosition};
+//         transform: translate(1px, -50%);
+
+//         &::after {
+//           top: 40%;
+//           right: ${directionStyle.arrowPosition};
+//           transform: translateX(1px) rotate(45deg);
+//           border-bottom-left-radius: ${directionStyle.arrowRadius};
+//         }
+//       `;
+//     default:
+//       return css``;
+//   }
+// };
+
+// const TooltipBox = styled.div<TooltipStylesProps>`
+//   position: relative;
+//   cursor: help;
+//   z-index: 10000;
+//   opacity: 0;
+
+//   ${({ direction, withArrow }) => directionStylesheet({ direction, withArrow })}
+
+//   position: absolute;
+//   visibility: hidden;
+//   width: max-content;
+//   height: max-content;
+
+//   background: ${color['main-black']};
+//   color: ${color.white};
+//   font-size: ${typography.size.xxs}px;
+//   font-weight: ${typography.weight.regular};
+//   padding: 7px 10px;
+//   border-radius: 7px;
+
+//   ${({ withArrow }) =>
+//     withArrow &&
+//     css`
+//       &::after {
+//         content: '';
+//         position: absolute;
+//         width: ${TOOLTIP_ARROW_WIDTH}px;
+//         height: ${TOOLTIP_ARROW_WIDTH}px; // arrow의 base인 정사각형 세로 height
+//         background: ${color['main-black']};
+//       }
+//     `}
+// `;
---
+++ src/stories/Tooltip.stories.tsx
@@ -0,0 +1,184 @@
+import { Meta, StoryFn } from '@storybook/react';
+
+import styled from '@emotion/styled';
+import Icon from '../components/Icon';
+import { Tooltip, Button, Badge } from '../components';
+import { B7 } from '../components';
+
+export default {
+  title: 'Components/Tooltip',
+  component: Tooltip,
+  tags: ['autodocs'],
+  decorators: [
+    (Story: StoryFn) => (
+      <Container>
+        <Story />
+      </Container>
+    ),
+  ],
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/tN7Q8dJZqVsjo2FWflOKfu/CDS(Corca-Design-System)?type=design&node-id=1013-35371&mode=design&t=ML3YPcDZJzLhfFhw-0',
+    },
+  },
+} as Meta<typeof Tooltip>;
+
+const Template: StoryFn<typeof Tooltip> = args => {
+  return (
+    <Tooltip {...args}>
+      <Icon.QuestionCircle />
+      {/* <Badge label="디자인시스테미은 짜증나" variant="green" /> */}
+    </Tooltip>
+  );
+};
+
+const TOOLTIP_CONTENT =
+  'Corca Design System TooltipCorca Design System TooltipCorca Design System TooltipCorca Design System TooltipCorca Design System TooltipCorca Design System TooltipCorca Design System TooltipCorca Design System TooltipCorca Design ';
+
+export const Basic = Template.bind({});
+Basic.args = {
+  direction: 'top',
+  withArrow: true,
+  content: TOOLTIP_CONTENT,
+};
+
+export const Arrow = () => {
+  return (
+    <ArrowContainer>
+      <ArrowContent>
+        <B7>Arrow Tooltip</B7>
+        <Tooltip direction="bottom" withArrow content="Arrow Tooltip">
+          <Icon.QuestionCircle />
+        </Tooltip>
+      </ArrowContent>
+
+      <ArrowContent>
+        <B7>No Arrow Tooltip</B7>
+        <Tooltip direction="bottom" withArrow={false} content="No Arrow Tooltip">
+          <Icon.QuestionCircle />
+        </Tooltip>
+      </ArrowContent>
+    </ArrowContainer>
+  );
+};
+
+// export const Direction = () => {
+//   return (
+//     <DirectionContainer>
+//       <DirectionContent>
+//         <Tooltip content={TOOLTIP_CONTENT} direction="top-start">
+//           <Button size="small" variant="filled">
+//             top start
+//           </Button>
+//         </Tooltip>
+//         <Tooltip content={TOOLTIP_CONTENT} direction="top">
+//           <Button size="small" variant="filled">
+//             top
+//           </Button>
+//         </Tooltip>
+//         <Tooltip content={TOOLTIP_CONTENT} direction="top-end">
+//           <Button size="small" variant="filled">
+//             top end
+//           </Button>
+//         </Tooltip>
+//       </DirectionContent>
+
+//       <DirectionContent>
+//         <DirectionLeftAndRightWrapper align="start">
+//           <Tooltip content={TOOLTIP_CONTENT} direction="left-start">
+//             <Button size="small" variant="filled">
+//               left start
+//             </Button>
+//           </Tooltip>
+//           <Tooltip content={TOOLTIP_CONTENT} direction="left">
+//             <Button size="small" variant="filled">
+//               left
+//             </Button>
+//           </Tooltip>
+//           <Tooltip content={TOOLTIP_CONTENT} direction="left-end">
+//             <Button size="small" variant="filled">
+//               left end
+//             </Button>
+//           </Tooltip>
+//         </DirectionLeftAndRightWrapper>
+
+//         <DirectionLeftAndRightWrapper align="end">
+//           <Tooltip content={TOOLTIP_CONTENT} direction="right-start">
+//             <Button size="small" variant="filled">
+//               right start
+//             </Button>
+//           </Tooltip>
+//           <Tooltip content={TOOLTIP_CONTENT} direction="right">
+//             <Button size="small" variant="filled">
+//               right
+//             </Button>
+//           </Tooltip>
+//           <Tooltip content={TOOLTIP_CONTENT} direction="right-end">
+//             <Button size="small" variant="filled">
+//               right end
+//             </Button>
+//           </Tooltip>
+//         </DirectionLeftAndRightWrapper>
+//       </DirectionContent>
+
+//       <DirectionContent>
+//         <Tooltip content={TOOLTIP_CONTENT} direction="bottom-start">
+//           <Button size="small" variant="filled">
+//             bottom start
+//           </Button>
+//         </Tooltip>
+//         <Tooltip content={TOOLTIP_CONTENT} direction="bottom">
+//           <Button size="small" variant="filled">
+//             bottom
+//           </Button>
+//         </Tooltip>
+//         <Tooltip content={TOOLTIP_CONTENT} direction="bottom-end">
+//           <Button size="small" variant="filled">
+//             bottom end
+//           </Button>
+//         </Tooltip>
+//       </DirectionContent>
+//     </DirectionContainer>
+//   );
+// };
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ArrowContainer = styled(Container)`
+  gap: 100px;
+`;
+
+const ArrowContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  align-items: center;
+`;
+
+const DirectionContainer = styled(Container)`
+  gap: 50px;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+  height: 700px;
+`;
+
+const DirectionContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 400px;
+`;
+
+const DirectionLeftAndRightWrapper = styled.div<{ align: 'end' | 'start' }>`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  align-items: ${({ align }) => `flex-${align}`};
+`;
