3ec88c3c61b9d6dbbe67bcab9c644eabcb00e785
dev-redo <mis05041@naver.com>
2024-03-31 17:54:21 +0900

    feat: implement color picker storybook

---
+++ src/stories/ColorPicker.stories.tsx
@@ -0,0 +1,38 @@
+import { StoryFn } from '@storybook/react';
+
+import styled from '@emotion/styled';
+
+import { ColorPicker } from '../components';
+import { useState } from 'react';
+
+export default {
+  title: 'Components/ColorPicker',
+  component: ColorPicker,
+  tags: ['autodocs'],
+  decorators: [
+    (Story: StoryFn) => (
+      <Container>
+        <Story />
+      </Container>
+    ),
+  ],
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/tN7Q8dJZqVsjo2FWflOKfu/CDS(Corca-Design-System)?type=design&node-id=8146-5295&mode=design&t=0N0xwK1u0rLH7slh-0',
+    },
+  },
+};
+
+export const Basic = () => {
+  const [color, setColor] = useState('#00a4ff');
+  return <ColorPicker color={color} onChangeColor={setColor} />;
+};
+
+const Container = styled.div`
+  width: 100%;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
