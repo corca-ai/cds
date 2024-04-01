3544957bb4dc04dce56640e72e4a52fba4c51f7a
NaGyeong-Park <97649363+NaGyeong-Park@users.noreply.github.com>
2024-04-01 15:35:37 +0900

    fix: fix storybook table radio header width

--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -176,7 +176,7 @@ export const Basic = () => {
           <Table.Th.Default width="100px" text="Badge" size={size} />
           <Table.Th.Default width="120px" text="Select" size={size} />
           <Table.Th.Default width="50px" text="Switch" size={size} />
-          <Table.Th.Default width="50px" text=" " size={size} />
+          <Table.Th.Default width={FIXED_TH_WIDTH.RADIO_ITEM[size]} text=" " size={size} />
           <Table.Th.Default width={FIXED_TH_WIDTH.KEBAB_MENU[size]} text=" " size={size} />
           <Table.Th.Checkbox
             checkboxType={{
@@ -264,7 +264,7 @@ export const ThWithIcon = () => {
                 <Table.Th.Default width="100px" text="Badge" size={size} />
                 <Table.Th.Default width="120px" text="Select" size={size} />
                 <Table.Th.Default width="50px" text="Switch" size={size} />
-                <Table.Th.Default width="50px" text=" " size={size} />
+                <Table.Th.Default width={FIXED_TH_WIDTH.RADIO_ITEM[size]} text=" " size={size} />
                 <Table.Th.Default width={FIXED_TH_WIDTH.KEBAB_MENU[size]} text=" " size={size} />
                 <Table.Th.Checkbox
                   checkboxType={{
@@ -362,7 +362,7 @@ export function Size() {
                 <Table.Th.Default width="100px" text="Label" size={th} />
                 <Table.Th.Default width="100px" text="Badge" size={th} />
                 <Table.Th.Default width="50px" text="Switch" size={th} />
-                <Table.Th.Default width="50px" text=" " size={th} />
+                <Table.Th.Default width={FIXED_TH_WIDTH.RADIO_ITEM[th]} text=" " size={th} />
                 <Table.Th.Default width={FIXED_TH_WIDTH.KEBAB_MENU[th]} text=" " size={th} />
                 <Table.Th.Checkbox
                   checkboxType={{
@@ -424,7 +424,7 @@ export function WithEllipsis() {
           <Table.Th.Default width="100px" text="Badge" size={thSize} />
           <Table.Th.Default width="120px" text="Select" size={thSize} />
           <Table.Th.Default width="50px" text="Switch" size={thSize} />
-          <Table.Th.Default width="50px" text=" " size={thSize} />
+          <Table.Th.Default width={FIXED_TH_WIDTH.RADIO_ITEM[thSize]} text=" " size={thSize} />
           <Table.Th.Default width={FIXED_TH_WIDTH.KEBAB_MENU[thSize]} text=" " size={thSize} />
           <Table.Th.Checkbox
             checkboxType={{
