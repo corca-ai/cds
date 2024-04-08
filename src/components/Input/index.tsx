526bb88e515d119a726c1875ccc28bd312dd2e28
SeungYeon Hong <hong009319@gmail.com>
2024-04-08 18:22:16 +0900

    ADCIO-3115) feat: implement file input (#37)
    * feat: implement file input
    
    * refactor: refactor file handler
    
    * feat: modify to accept more mime types
    
    * docs: modify storybook
    
    * feat: add file name to input
    
    * feat: export file input
    
    * refactor: rollback input container style
    
    ---------
    
    Co-authored-by: dev-redo <mis05041@naver.com>

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,12 +3,14 @@ import { NumberInput, NumberInputProps } from './NumberInput';
 import { PasswordInput, PasswordInputProps } from './PasswordInput';
 import { TextInput, TextInputProps } from './TextInput';
 import { ColorPickerInput, ColorPickerInputProps } from './ColorPickerInput';
+import { FileInput, FileInputProps } from './FileInput';
 
-export { InputContainer, TextInput, PasswordInput, NumberInput, ColorPickerInput };
+export { InputContainer, TextInput, PasswordInput, NumberInput, ColorPickerInput, FileInput };
 export type {
   InputBaseProps,
   TextInputProps,
   PasswordInputProps,
   NumberInputProps,
   ColorPickerInputProps,
+  FileInputProps,
 };
