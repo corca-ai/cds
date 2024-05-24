cd2c1ba7daf2f3e63117b05c31169c95c2488f70
SeungYeon Hong <lisa@corca.ai>
2024-05-24 16:50:20 +0900

    NONE) chore: modify rollup options (#65)
    * chore: modify rollup options
    
    * chore: remove vite-react and emotion babel
    
    ---------
    
    Co-authored-by: dev-redo <mis05041@naver.com>

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,12 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
   },
   plugins: [dts()],
