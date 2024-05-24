5220156f5da562b9868320f5e6619cb6e6091e84
SeungYeon Hong <lisa@corca.ai>
2024-05-24 17:38:53 +0900

    NONE) chore: modify format (#67)
    * chore: modify format
    
    * release: upgrade cds 0.0.8
    
    ---------
    
    Co-authored-by: dev-redo <mis05041@naver.com>

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
-      formats: ['es'],
+      formats: ['es', 'cjs'],
       name: 'cds',
       fileName: 'index',
     },
