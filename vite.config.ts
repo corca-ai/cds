841e03bd56bb29633e56a68cd9b388daa7413d43
dev-redo <mis05041@naver.com>
2024-05-24 17:11:14 +0900

    chore: modify vite config to use react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   build: {
@@ -10,15 +11,6 @@ export default defineConfig({
       name: 'cds',
       fileName: 'index',
     },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
-    },
   },
-  plugins: [dts()],
+  plugins: [react(), dts()],
 });
