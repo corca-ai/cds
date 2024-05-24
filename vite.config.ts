92a58f668e29176e8a547e7a6809aa5e733947ea
dev-redo <mis05041@naver.com>
2024-05-24 16:47:09 +0900

    chore: remove vite-react and emotion babel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   build: {
@@ -21,13 +20,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
-    dts(),
-  ],
+  plugins: [dts()],
 });
