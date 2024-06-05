f20eabbc4b229b3d15839ff1f7e479a46d88d485
dev-redo <mis05041@naver.com>
2024-06-05 17:05:14 +0900

    feat: init

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,10 @@
-import "../src/global.css";
-import "./styles.css";
+import '../src/global.css';
+import './styles.css';
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+
 import dts from 'vite-plugin-dts';
 import react from '@vitejs/plugin-react';
+// import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 export default defineConfig({
   build: {
+    minify: false,
     lib: {
       entry: resolve(__dirname, './src/index.ts'),
       formats: ['es', 'cjs'],
@@ -20,6 +23,7 @@ export default defineConfig({
         },
       },
     },
+    minify: false,
   },
   plugins: [react(), dts()],
 });
