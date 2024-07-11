319c60896d19bfc356eeef9e47fc9874df34e117
younggwon <younggwon@corca.ai>
2024-07-11 14:29:16 +0900

    fix: update Storybook configuration to use glob pattern for stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,14 @@
 module.exports = {
-  framework: "@storybook/react-vite",
-  stories: ["../src/stories/*.stories.@(js|jsx|ts|tsx)"],
+  framework: '@storybook/react-vite',
+  stories: ['../**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/addon-storysource",
-    "@storybook/addon-designs",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-storysource',
+    '@storybook/addon-designs',
   ],
   core: {
-    builder: "@storybook/builder-vite",
+    builder: '@storybook/builder-vite',
   },
 };
