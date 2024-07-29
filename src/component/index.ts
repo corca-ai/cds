bfa605e9e96687b03e9f7c39c351df5ec2088d66
dev-redo <mis05041@naver.com>
2024-07-29 19:43:59 +0900

    feat: export switch component

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,4 +1,8 @@
 export { Button } from './Button/button';
 export type { IButtonProps } from './Button/button';
+
 export { Badge } from './Badge/Badge';
 export type { BadgeProps } from './Badge/Badge';
+
+export { Switch } from './Switch/Switch';
+export type { SwitchProps } from './Switch/Switch';
