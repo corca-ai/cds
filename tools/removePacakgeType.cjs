416759263de183dc60591e22bced997140938d80
younggwon <younggwon@corca.ai>
2024-08-13 17:23:08 +0900

    chore: remove "type" key from package.json

---
+++ tools/removePacakgeType.cjs
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const fs = require('fs');
+const path = require('path');
+
+const packagePath = path.resolve(__dirname, '..', 'package.json');
+
+fs.readFile(packagePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const packageJson = JSON.parse(data);
+  delete packageJson.type;
+
+  fs.writeFile(packagePath, JSON.stringify(packageJson, null, 2), 'utf8', err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('The "type" key has been removed from the package.json file.');
+  });
+});
